061fd93615a7138b4699eec8ea428fb012c36e76
KiyonamiYu <514723273@qq.com>
2019-12-05 11:59:38 +0800

    feat: 添加 prettier and eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,11 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    "plugin:prettier/recommended",
-    'airbnb',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module',
+		ecmaFeatures: { jsx: true },
+	},
+	extends: ['eslint:recommended', 'prettier', 'plugin:react/recommended'],
+	plugins: ['prettier', 'react'],
+	rules: { 'prettier/prettier': ['error'] },
+	env: { browser: true, node: true, mocha: false, jest: false },
 };
