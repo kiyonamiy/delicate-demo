3949f02194def6943bb0edcf70057ec3243dfa21
KiyonamiYu <514723273@qq.com>
2019-12-19 17:43:54 +0800

    fix: 修改eslintrc配置导致ts报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
 	parserOptions: {
-		ecmaVersion: 2017,
+		ecmaVersion: 2019,
 		sourceType: 'module',
 		ecmaFeatures: { jsx: true },
 	},
-	extends: ['eslint:recommended', 'prettier', 'plugin:react/recommended'],
+	extends: [
+		'prettier',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+	],
 	plugins: ['prettier', 'react'],
 	rules: { 'prettier/prettier': ['error'] },
 	env: { browser: true, node: true, mocha: false, jest: false },
--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Button } from 'antd';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <Button type="primary">Helllo World!</Button>
-    </div>
-  );
-}
+	return (
+		<div>
+			<Button type="primary">Helllo World!</Button>
+		</div>
+	);
+};
 
 export default App;
